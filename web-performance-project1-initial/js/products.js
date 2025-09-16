00bc2cf072897508c747f190c217088d30fb09d7
manhg <manhg@zigexn.vn>
2025-09-16 20:42:07 +0700

    test githook

--- web-performance-project1-initial/js/products.js
+++ web-performance-project1-initial/js/products.js
@@ -66,3 +66,8 @@ function displayProducts(products) {
 
 
 loadProducts();
+
+// Simulate heavy operation. It could be a complex price calculation.
+for (let i = 0; i < 10000000; i++) {
+    const temp = Math.sqrt(i) * Math.sqrt(i);
+}
